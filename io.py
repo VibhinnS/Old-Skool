5ac02afb0c72874d3459ebb33785155fb3bb4cab
VibhinnS <vibhinnsinghal25@gmail.com>
2024-04-23 23:37:21 +0530

    Added carry flag opcodes and new opcde class

--- io.py
+++ io.py
@@ -13,7 +13,7 @@ class CHIP8(pyglet.window.Window):
 
     memory = [0] * 4960  # 4096 bytes of memory
     display_buffer = [0] * 32 * 64  # 64x32 display buffer
-    gpio = [0] * 16  # 16 8-bit registers
+    register_bank = [0] * 16  # 16 8-bit registers
     sound_timer = 0  # sound timer
     delay_timer = 0  # delay timer
     index = 0  # 16 bit index register
@@ -27,13 +27,14 @@ class CHIP8(pyglet.window.Window):
     def main(self) -> None:
         self.initialize()
         self.load_rom(sys.argv[1])
-        self.cycle()
-        self.draw()
+        while not self.has_exit:
+            self.cycle()
+            self.draw()
 
     def initialize(self) -> None:
         self.clear()
         self.memory = [0]*4096
-        self.gpio = [0]*16
+        self.register_bank = [0]*16
         self.display_buffer = [0]*32*64
         self.stack = []
         self.Key_inputs = [0]*16
@@ -58,7 +59,7 @@ class CHIP8(pyglet.window.Window):
         binary = open(ROM_PATH, "rb").read()
         i :int = 0
         while i < len(binary):
-            self.memory[i + 512] = binary[i]
+            self.memory[i + 512] = ord(binary[i])
             i += 1
 
     def cycle(self):
@@ -68,11 +69,11 @@ class CHIP8(pyglet.window.Window):
         self.vy = (self.opcode & 0x00F0) >> 4
         self.pc += 2
 
-        extracted_op = self.opcode & 0xf000
-        try:
-            self.funcmap[extracted_op]()
-        except:
-            print("Unknown opcode: %s", self.opcode)
+        # extracted_op = self.opcode & 0xf000
+        # try:
+        #     self.funcmap[extracted_op]()
+        # except:
+        #     print("Unknown opcode: %s", self.opcode)
 
         if self.delay_timer > 0:
             self.delay_timer -= 1
@@ -80,27 +81,5 @@ class CHIP8(pyglet.window.Window):
             self.sound_timer -= 1
             if self.sound_timer == 0:
                 print("BEEP")
-
-    #All the OpCodes listed here - 
-
-    def _0ZZZ(self):
-        extracted_op = self.opcode & 0xf0ff
-        try:
-            self.funcmap[extracted_op]()
-        except:
-            print("Unknown opcode: %s", self.opcode)
-
-    def _0ZZ0(self):
-        logging.log("Clearing Screen")
-        self.display_buffer = [0]*364*32
-        self.should_draw = True
-
-    def _0ZZE(self):
-        logging.log("Returning from subroutine")
-        self.pc = self.stack.pop()
-
-    def _1ZZZ(self):
-        logging.log("Jumping addresses")
-        self.pc = self.opcode & 0x0fff
-
+                
     
--- opcodes.py
+++ opcodes.py
@@ -1,31 +1,69 @@
 import logging
+from functools import partial
+from typing import BinaryIO, Any
 
 class Opcodes:
     def __init__(self, chip8):
         self.chip8 = chip8
         self.funcmap = {
-            0x0000: self._0ZZZ,
-            0x00e0: self._0ZZ0,
-            0x00ee: self._0ZZE,
-            0x1000: self._1ZZZ,
+            0x0000: partial(self._0ZZZ, chip8),
+            0x00e0: partial(self._0ZZ0, chip8),
+            0x00ee: partial(self._0ZZE, chip8),
+            0x1000: partial(self._1ZZZ, chip8),
         }
 
-    def _0ZZZ(self):
-        extracted_op = self.chip8.opcode & 0xf0ff
+    """Instrxns which take into account the leftmost nibbles of the opcode"""
+    def _0ZZZ(self, chip8) -> None:
+        extracted_op = chip8.opcode & 0xf0ff
         try:
             self.funcmap[extracted_op]()
         except KeyError:
-            print("Unknown opcode: {}".format(hex(self.chip8.opcode)))
+            print("Unknown opcode: {}".format(hex(chip8.opcode)))
 
-    def _0ZZ0(self):
+
+    def _4ZZZ(self, chip8) -> None:
+        """For instructions that use VX register when it's not equal to NN"""
+        if chip8.register_bank[chip8.vx] != (chip8.opcode & 0x00ff):
+            chip8.pc += 2
+
+
+    def _5ZZZ(self, chip8) -> None:
+        """For instrxns when VX == Vy register"""
+        if chip8.register_bank[chip8.vx] == chip8.register_bank[chip8.vy]:
+            chip8.pc += 2
+
+
+    def _8ZZ4(self, chip8) -> None:
+        if chip8.register_bank[chip8.vx] + chip8.register_bank[chip8.vy] > 0xff:
+            chip8.register_bank[0xf] = 1
+        else:
+            chip8.register_bank[0xf] = 0
+        chip8.register_bank[chip8.vx] += chip8.register_bank[chip8.vy]
+        chip8.register_bank[chip8.vx] &= 0xff 
+
+    def _8ZZ5(self, chip8):
+        if chip8.register_bank[chip8.vy] > chip8.register_bank[chip8.vx]:
+            chip8.register_bank[0xf] = 0
+        else:
+            chip8.register_bank[0xf] = 1
+
+        chip8.register_bank[chip8.vx] -= chip8.register_bank[chip8.vy]
+        chip8.register_bank[chip8.vx] &= 0xff
+
+
+    def _0ZZ0(self, chip8) -> None:
         logging.log(logging.INFO, "Clearing Screen")
-        self.chip8.display_buffer = [0] * 64 * 32
-        self.chip8.should_draw = True
+        chip8.display_buffer = [0] * 64 * 32
+        chip8.should_draw = True
 
-    def _0ZZE(self):
+
+    def _0ZZE(self, chip8) -> None:
         logging.log(logging.INFO, "Returning from subroutine")
-        self.chip8.pc = self.chip8.stack.pop()
+        chip8.pc = chip8.stack.pop()
 
-    def _1ZZZ(self):
+
+    def _1ZZZ(self, chip8) -> None:
         logging.log(logging.INFO, "Jumping addresses")
-        self.chip8.pc = self.chip8.opcode & 0x0fff
+        chip8.pc = chip8.opcode & 0x0fff
+
+
